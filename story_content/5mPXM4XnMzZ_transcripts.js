8cda53a61d115de1d75e05dd16d720eb6f5b7e41
njose314 <iamnaveenjose@outlook.com>
2025-12-26 18:10:56 +0530

    Add files via upload

---
+++ story_content/5mPXM4XnMzZ_transcripts.js
@@ -0,0 +1,5 @@
+
+(function() {
+    const data = {"transcripts":[{"name":"captions","cues":[{"start":9,"text":"A paper jam."},{"start":1169,"text":"Are you serious?"},{"start":2289,"text":"I need these reports now,"},{"start":3650,"text":"not in an hour."},{"start":4659,"text":"This is completely unacceptable."},{"start":6570,"text":"Fix it."}]}]};
+    window.globalLoadJsAsset('story_content/5mPXM4XnMzZ_transcripts.js', JSON.stringify(data));
+})();
